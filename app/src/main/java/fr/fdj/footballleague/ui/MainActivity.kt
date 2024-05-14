b924ae3e99dacd37f1788844b8ca48728c7266f2
Gaëtan Herfray <gherfray@tutanota.com>
2024-05-14 19:06:22 +0200

    Making the app runnable by adding an empty MainActivity with a matching declaration in the Manifest

---
+++ app/src/main/java/fr/fdj/footballleague/ui/MainActivity.kt
@@ -0,0 +1,10 @@
+package fr.fdj.footballleague.ui
+
+import androidx.activity.ComponentActivity
+
+/**
+ * Activity which displays the main screen of the application, which consists of a search form and
+ * the list of teams matching the typed league in the form.
+ */
+class MainActivity: ComponentActivity() {
+}
